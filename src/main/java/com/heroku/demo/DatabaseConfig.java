0f6d5f998da1bd8dfc29741abe65e857c5c47f7f
Amit Jambotkar <amit.j@ezetap.com>
2016-02-17 22:15:11 +0530

    Initial Commit

--- src/main/java/com/heroku/demo/DatabaseConfig.java
+++ src/main/java/com/heroku/demo/DatabaseConfig.java
@@ -1,11 +1,11 @@
 package com.heroku.demo;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
@@ -15,4 +15,6 @@ public class DatabaseConfig {
     public DataSource dataSource() {
         return new org.apache.tomcat.jdbc.pool.DataSource();
     }
+    
+   
 }
